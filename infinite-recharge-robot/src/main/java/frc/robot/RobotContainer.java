98d961f86aae254a506e78ce1265412dbca40eb9
Adam Brightwell <adam.brightwell@gmail.com>
2020-01-12 20:30:08 -0500

    drive system cleanup and updates
    - cleaned up some of the code.
    - added peak output configuration for master controllers.
    - removed 'example' subsystem and command.

--- infinite-recharge-robot/src/main/java/frc/robot/RobotContainer.java
+++ infinite-recharge-robot/src/main/java/frc/robot/RobotContainer.java
@@ -10,18 +10,17 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.DriveSystem;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 
 /**
- * This class is where the bulk of the robot should be declared.  Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
- * (including subsystems, commands, and button mappings) should be declared here.
+ * This class is where the bulk of the robot should be declared.  Since
+ * Command-based is a "declarative" paradigm, very little robot logic should
+ * actually be handled in the {@link Robot} periodic methods (other than the
+ * scheduler calls).  Instead, the structure of the robot (including subsystems,
+ * commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
@@ -30,27 +29,29 @@ public class RobotContainer {
   Joystick controller = new Joystick(1);
 
   private final Command driveCommand = new RunCommand(
-    () -> this.driveSystem.driveVelocity(
-      this.joystick.getRawAxis(1),
-      this.controller.getRawAxis(1)
+    () -> this.driveSystem.tank(
+        this.joystick.getRawAxis(1),
+        this.controller.getRawAxis(1)
       ),  
     this.driveSystem
     );
   
   /**
-   * The container for the robot.  Contains subsystems, OI devices, and commands.
+   * The container for the robot.  Contains subsystems, OI devices, and
+   * commands.
    */
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
     configureDefaultCommands();
-    this.driveSystem.setPIDFValues(.15, 0, 0, 0);
+    this.driveSystem.setPIDF(0.15, 0, 0, 0);
   }
 
   /**
-   * Use this method to define your button->command mappings.  Buttons can be created by
-   * instantiat ing a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
+   * Use this method to define your button->command mappings.  Buttons can be
+   * created by instantiat ing a {@link GenericHID} or one of its subclasses
+   * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and
+   * then passing it to a
    * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
--- infinite-recharge-robot/src/main/java/frc/robot/subsystems/DriveSystem.java
+++ infinite-recharge-robot/src/main/java/frc/robot/subsystems/DriveSystem.java
@@ -11,7 +11,6 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.FollowerType;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.kauailabs.navx.frc.AHRS;
@@ -19,61 +18,68 @@ import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.SPI.Port;
 
 public class DriveSystem extends SubsystemBase {
-  /**
-   * Creates a new DriveSystem.
-   */  
-  TalonSRX rightMaster;
-  TalonSRX rightSlave;
-  TalonSRX leftMaster;
-  TalonSRX leftSlave;
+  private TalonSRX rightMaster;
+  private TalonSRX rightSlave;
+  private TalonSRX leftMaster;
+  private TalonSRX leftSlave;
   private AHRS navX;
   private Port navXPort;
-  private double targetL = 0, targetR = 0;
+
   public static double MAX_VELOCITY = 200;
+
+  private static final double PEAK_OUTPUT = 1.0;
+
   public DriveSystem() {
-    rightMaster = new TalonSRX(Constants.RIGHT_MASTER_MOTOR);
-    rightSlave = new TalonSRX(Constants.RIGHT_SLAVE_MOTOR);
-    leftMaster = new TalonSRX(Constants.LEFT_MASTER_MOTOR);
-    leftSlave = new TalonSRX(Constants.LEFT_SLAVE_MOTOR);
-    navXPort = SPI.Port.kMXP;
-    navX = new AHRS(navXPort);
+    // Initialize all of the drive systems motor controllers.
+    this.leftMaster = new TalonSRX(Constants.LEFT_MASTER_MOTOR);
+    this.leftSlave = new TalonSRX(Constants.LEFT_SLAVE_MOTOR);
+    this.rightMaster = new TalonSRX(Constants.RIGHT_MASTER_MOTOR);
+    this.rightSlave = new TalonSRX(Constants.RIGHT_SLAVE_MOTOR);
 
-    rightSlave.follow(rightMaster, FollowerType.AuxOutput1);
-    leftSlave.follow(leftMaster, FollowerType.AuxOutput1);
+    this.leftSlave.follow(leftMaster, FollowerType.AuxOutput1);
+    this.rightSlave.follow(rightMaster, FollowerType.AuxOutput1);
 
-    rightMaster.setSensorPhase(true);
+    this.leftMaster.setInverted(true);
+    this.rightMaster.setSensorPhase(true);
 
-    leftMaster.setInverted(true);
-    leftSlave.setInverted(true);
+    this.leftMaster.configPeakOutputForward(PEAK_OUTPUT);
+    this.leftMaster.configPeakOutputReverse(PEAK_OUTPUT);
+
+    this.rightMaster.configPeakOutputForward(PEAK_OUTPUT);
+    this.rightMaster.configPeakOutputReverse(PEAK_OUTPUT);
+
+    // Initialize the NavX IMU sensor.
+    this.navXPort = SPI.Port.kMXP;
+    this.navX = new AHRS(navXPort);
   }
 
-  public double getAngle() {
-    return navX.getAngle();
+  public void setPIDF(double p, double i, double d, double f){
+    // Set PIDF for Left Master controller.
+    this.leftMaster.config_kP(0, p, 100);
+    this.leftMaster.config_kI(0, i, 100 );
+    this.leftMaster.config_kD(0, d, 100 );
+    this.leftMaster.config_kF(0, f, 100 );
+
+    // Set PIDF for Right Master controller.
+    this.rightMaster.config_kP(0, p, 100);
+    this.rightMaster.config_kI(0, i, 100);
+    this.rightMaster.config_kD(0, d, 100);
+    this.rightMaster.config_kF(0, f, 100);
   }
 
-  public void setPIDFValues(double p, double i, double d, double f){
-    rightMaster.config_kP(0, p, 100);
-    leftMaster.config_kP(0, p, 100);
-    rightMaster.config_kI(0, i, 100);
-    leftMaster.config_kI(0, i, 100 );
-    rightMaster.config_kD(0, d, 100);
-    leftMaster.config_kD(0, d, 100 );
-    rightMaster.config_kF(0, f, 100);
-    leftMaster.config_kF(0, f, 100 );
-  }
+  public void tank(double left, double right) {
+    double targetLeft = left * MAX_VELOCITY * 4096/600.0;
+    double targetRight = right * MAX_VELOCITY * 4096/600.0;
 
-  public void driveVelocity(double left, double right){
-    System.out.println("Left: " + targetL + " " + "Right: " + targetR);
-    targetL = left * MAX_VELOCITY * 4096/600.0;
-    targetR = right * MAX_VELOCITY * 4096/600.0;
-    rightMaster.set(ControlMode.Velocity, targetR);
-    leftMaster.set(ControlMode.Velocity, targetL);
+    this.leftMaster.set(ControlMode.Velocity, targetLeft);
+    this.rightMaster.set(ControlMode.Velocity, targetRight);
   }
 
   @Override
   public void periodic() {
     System.out.printf("Left: %d - Right: %d\n",
-    leftMaster.getSelectedSensorVelocity(0), 
-    rightMaster.getSelectedSensorVelocity(0));
+      this.leftMaster.getSelectedSensorVelocity(0),
+      this.rightMaster.getSelectedSensorVelocity(0)
+    );
   }
 }
