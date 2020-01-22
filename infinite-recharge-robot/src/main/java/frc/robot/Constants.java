8b4a41409773ab6b02633f9d0b8be010cd9e2e34
Adam Brightwell <adam.brightwell@gmail.com>
2020-01-22 11:11:34 -0500

    add controller button constants

--- infinite-recharge-robot/src/main/java/frc/robot/Constants.java
+++ infinite-recharge-robot/src/main/java/frc/robot/Constants.java
@@ -23,10 +23,23 @@ public final class Constants {
     public static final int DRIVER_CONTROLLER = 0;
     public static final int INTAKE_MOTOR = 6;
     public static final int CONVEYOR_MOTOR = 7;
-
+    public static final int SWING_MOTOR = 8;
+    public static final int POTEN = 5;
     public static final int POWER_CELL_1 = 0;
     public static final int POWER_CELL_2 = 1;
     public static final int POWER_CELL_3 = 2;
     public static final int POWER_CELL_4 = 3;
     public static final int POWER_CELL_5 = 4;
+    public static final int A_BUTTON = 2;
+    public static final int B_BUTTON = 3;
+    public static final int Y_BUTTON = 4;
+    public static final int X_BUTTON = 1;
+    public static final int START_BUTTON = 10;
+    public static final int BACK_BUTTON = 9;
+    public static final int RIGHT_TRIGGER = 8;
+    public static final int LEFT_TRIGGER = 7;
+    public static final int RIGHT_BUMPER =6;
+    public static final int LEFT_BUMPER =5;
+    public static final int RIGHT_JOYSTICK_BUTTON = 12;
+    public static final int LEFT_JOYSTICK_BUTTON = 11;
 }
